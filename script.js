398df18b08daf13904eb5b0c1d73b2aa0df40fce
ahmad123jamal <you@example.com>
2024-09-29 12:50:39 +0500

    completed

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-var dateOfBirth = document.getElementById('DOB');
+var dateOfBirth = document.querySelector('#DOB');
 
-var calbtn = document.getElementById('Calculate');
+
+var calbtn = document.querySelector('#Calculate');
 
 var year = document.getElementById('Years');
 
 var month = document.getElementById('Months');
 
 var day = document.getElementById('Day');
+
+ calbtn.addEventListener("click", function () {
+    var birthDate = new Date(dateOfBirth.value);
+    var currentDate = new Date();
+
+    var years = currentDate.getFullYear() - birthDate.getFullYear();
+    var months = currentDate.getMonth() - birthDate.getMonth();
+    var days = currentDate.getDay() - birthDate.getDay();
+
+
+
+
+
+if ( days < 0) {
+    months--;
+    days += 30;
+}
+if (months < 0) {
+    years--;
+    month += 12;
+}
+
+year.textContent = years;
+month.textContent = months;
+day.textContent = days;
+
+
+})
+ 
